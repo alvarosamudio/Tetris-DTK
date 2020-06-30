a1fac1a169b4f407f1e6533f0387a095a6fecbb6
alvarosamudio <alvarosamudio@protonmail.com>
2020-06-30 00:51:21 -0500

    Update main widget and UI layout for native integration

--- src/widget.cpp
+++ src/widget.cpp
@@ -1,14 +1,26 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QVBoxLayout>
 
-Widget::Widget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Widget)
-{
-    ui->setupUi(this);
+Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget) {
+  ui->setupUi(this);
+
+  m_gameBoard = new GameBoard(this);
+
+  // Create a layout for the gameContainer if it doesn't have one
+  if (!ui->gameContainer->layout()) {
+    ui->gameContainer->setLayout(new QVBoxLayout());
+  }
+  ui->gameContainer->layout()->addWidget(m_gameBoard);
+  ui->gameContainer->layout()->setContentsMargins(0, 0, 0, 0);
+
+  connect(ui->startBtn, &QPushButton::clicked, this, &Widget::onStartClicked);
 }
 
-Widget::~Widget()
-{
-    delete ui;
+Widget::~Widget() { delete ui; }
+
+void Widget::onStartClicked() {
+  m_gameBoard->startGame();
+  m_gameBoard->setFocus();
+  ui->startBtn->setText("Restart");
 }
--- src/widget.h
+++ src/widget.h
@@ -1,22 +1,26 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+#include "gameboard.h"
 #include <QWidget>
 
 namespace Ui {
 class Widget;
 }
 
-class Widget : public QWidget
-{
-    Q_OBJECT
+class Widget : public QWidget {
+  Q_OBJECT
 
 public:
-    explicit Widget(QWidget *parent = 0);
-    ~Widget();
+  explicit Widget(QWidget *parent = 0);
+  ~Widget();
+
+private slots:
+  void onStartClicked();
 
 private:
-    Ui::Widget *ui;
+  Ui::Widget *ui;
+  GameBoard *m_gameBoard;
 };
 
 #endif // WIDGET_H
